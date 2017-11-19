a699ba6a092f2461ec1c082834e8342be8083bcb
Aen <hello@aentan.com>
2017-11-20 00:01:43 +0800

    layout and styles

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,116 +1,135 @@
-var $body;
-var $HeroHeader;
-var $IHeader;
+/** Set up relative positions and scales **/
+var VIEW = {};
+VIEW.width = window.innerWidth;
+VIEW.height = window.innerHeight;
+VIEW.centerX = VIEW.width / 2;
+VIEW.centerY = VIEW.height / 2;
+VIEW.offsetX = VIEW.width / 2;
+VIEW.offsetY = VIEW.height / 2;
 
-var CWHeaderPosOrig;
+// Matter.js module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Common = Matter.Common,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse;
 
-$(function() {
+// create engine
+var engine = Engine.create(),
+    world = engine.world;
 
-  $body       = $("body");
-  $HeroHeader    = $(".hero-header");
-
-  $CWSection   = $(".client-work");
-  $CWHeader   = $(".client-work-header");
-  $IHeader    = $(".ideas-header");
-
-  function adjustHeroHeader() {
-    // Main Header
-    if ($body.scrollTop() < 34) {
-      $HeroHeader.height(90 - $body.scrollTop());
-    } else {
-      $HeroHeader.height('56px');
-    }
-    if ($body.scrollTop() > 0) {
-      $HeroHeader.addClass('opaque');
-    } else {
-      $HeroHeader.removeClass();
-    }
-  };
-
-  $('section > header').addClass('locked-bottom');
-
-  function adjustSectionHeaders() {
-    $('section').each(function() {
-      // Section scrolled into viewport
-      if ($(this).offset().top - $body.scrollTop() >= $(window).height() - $(this).find('header').height()) {
-        $(this).find('header').addClass('locked-bottom');
-      } else {
-        $(this).find('header').removeClass('locked-bottom');
-      }
-      // Section scrolled to top
-      // if ($(this).offset().top - $body.scrollTop() < 0) {
-      //   $(this).find('header').addClass('locked-top');
-      // } else {
-      //   $(this).find('header').removeClass('locked-top');
-      // }
-    });
-  };
-
-  $(window).scroll($.throttle(10, adjustHeroHeader));
-  $(window).scroll($.throttle(50, adjustSectionHeaders));
-
-  adjustHeroHeader();
-  adjustSectionHeaders();
-
-  // Sticky Goto
-  $(".goto nav").stick_in_parent({
-    offset_top: 167
-  });
-
-
-  //initialize swiper
-  var experienceSwiper = new Swiper ('.experience .swiper-container', {
-    freeMode: true,
-    freeModeSticky: true,
-    freeModeMinimumVelocity: .05,
-    freeModeMomentumRatio: .25,
-    freeModeMomentumVelocityRatio: 1.5,
-    slidesPerView: 'auto',
-    slidesOffsetBefore: 30,
-    slidesOffsetAfter: 30,
-    spaceBetween: 15,
-    grabCursor: true,
-    prevButton: '.swiper-button-prev',
-    nextButton: '.swiper-button-next'
-  });
-  var clientWorkSwiper = new Swiper ('.client-work .swiper-container', {
-    freeMode: true,
-    freeModeSticky: true,
-    freeModeMinimumVelocity: .05,
-    freeModeMomentumRatio: .25,
-    freeModeMomentumVelocityRatio: 1.5,
-    slidesPerView: 'auto',
-    slidesOffsetBefore: 30,
-    slidesOffsetAfter: 30,
-    spaceBetween: 15,
-    pagination: '.swiper-pagination',
-    grabCursor: true,
-    prevButton: '.swiper-button-prev',
-    nextButton: '.swiper-button-next'
-  });
-
-  // Parallax
-  $('.hero').parallaxScroll({
-    friction: 0.5
-  });
-  $(window).trigger('resize');
-
-  // Goto nav builder
-  $('.single article').anchorific({
-    navigation: '.goto nav'
-  });
-
-  // Scroll to
-  $('a[href^="#"]').click(function(e) {
-    e.preventDefault();
-    var duration = Math.abs($(this.hash).offset().top - $body.scrollTop()) * .75;
-    duration = (duration > 1000) ? 1000 : duration;
-    var offset = this.hash == "#client-work" ? $(this).height() * -2 : $(this).height() * -1;
-    $(window).stop(true).scrollTo(this.hash, {
-      duration: duration,
-      offset: offset,
-      interrupt: false
-    });
-  });
-
+// create renderer
+var render = Render.create({
+  engine: engine,
+  element: document.getElementById("debug"),
+  options: {
+    width: window.innerWidth,
+    height: window.innerWidth,
+    background: 'transparent',
+    wireframeBackground: 'transparent',
+    hasBounds: false,
+    enabled: true,
+    wireframes: true,
+    showSleeping: true,
+    showDebug: false,
+    showBroadphase: false,
+    showBounds: false,
+    showVelocity: false,
+    showCollisions: false,
+    showAxes: false,
+    showPositions: false,
+    showAngleIndicator: false,
+    showIds: false,
+    showShadows: false
+  }
 });
+
+// Render.run(render);
+
+// create runner
+var runner = Runner.create();
+Runner.run(runner, engine);
+
+// add walls
+var wallopts = {
+    isStatic: true,
+    restitution: 0.2,
+    friction: 1
+};
+var groundopts = {
+    isStatic: true,
+    restitution: 0,
+    friction: 1
+};
+World.add(world, [
+  // ground
+  Bodies.rectangle(window.innerWidth/2, window.innerHeight+50, window.innerWidth, 100, groundopts),
+  // walls
+  Bodies.rectangle(window.innerWidth/2, -50, window.innerWidth, 100, wallopts), // top
+  Bodies.rectangle(window.innerWidth+50, window.innerHeight/2, 100, window.innerHeight, wallopts), // right
+  Bodies.rectangle(-50, window.innerHeight/2, 100, window.innerHeight, wallopts) // left
+]);
+
+var bodiesDom = document.querySelectorAll('.strip');
+var bodies = [];
+for (var i = 0, l = bodiesDom.length; i < l; i++) {
+  var body = Bodies.rectangle(
+    VIEW.centerX + Math.floor(Math.random() * VIEW.width/2) - VIEW.width/4,
+    0,
+    VIEW.width * bodiesDom[i].offsetWidth / window.innerWidth,
+    VIEW.height * bodiesDom[i].offsetHeight / window.innerHeight, {
+      restitution: 0.2,
+      friction: 1
+    }
+  );
+  bodiesDom[i].id = body.id;
+  bodies.push(body);
+}
+World.add(engine.world, bodies);
+
+// add mouse control
+var mouse = Mouse.create(render.canvas),
+  mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 1,
+      render: {
+        visible: false
+      }
+    }
+  });
+
+World.add(engine.world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+
+window.requestAnimationFrame(update);
+
+function update() {
+  for (var i = 0, l = bodiesDom.length; i < l; i++) {
+    var bodyDom = bodiesDom[i];
+    var body = null;
+    for (var j = 0, k = bodies.length; j < k; j++) {
+      if (bodies[j].id == bodyDom.id) {
+        body = bodies[j];
+        break;
+      }
+    }
+
+    if (body === null) continue;
+
+    bodyDom.style.transform = "translate( " +
+      (body.position.x - bodyDom.offsetWidth / 2) +
+      "px, " +
+      (body.position.y - bodyDom.offsetHeight / 2) +
+      "px )";
+    bodyDom.style.transform += "rotate( " + body.angle + "rad )";
+
+  }
+  window.requestAnimationFrame(update);
+}
