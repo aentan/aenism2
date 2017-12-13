51a20c1a39da0c486161381534e808a9ca83731d
Aen <hello@aentan.com>
2017-12-13 14:06:30 +0800

    Publishing to gh-pages (deploy.sh)

--- src/js/main.js
+++ src/js/main.js
@@ -1,116 +1,281 @@
-var $body;
-var $HeroHeader;
-var $IHeader;
+function makeWorld() {
 
-var CWHeaderPosOrig;
+  // Should reappear
+  document.getElementsByTagName("main")[0].style.visibility = "visible";
 
-$(function() {
+  /** Set up relative positions and scales **/
+  var VIEW = {};
+  VIEW.width    = window.innerWidth;
+  VIEW.height   = window.innerHeight;
+  VIEW.centerX  = VIEW.width / 2;
+  VIEW.centerY  = VIEW.height / 2;
+  VIEW.offsetX  = VIEW.width / 2;
+  VIEW.offsetY  = VIEW.height / 2;
 
-  $body       = $("body");
-  $HeroHeader    = $(".hero-header");
+  // Matter.js module aliases
+  var Engine    = Matter.Engine,
+      Render    = Matter.Render,
+      Runner    = Matter.Runner,
+      Common    = Matter.Common,
+      World     = Matter.World,
+      Bodies    = Matter.Bodies,
+      Body      = Matter.Body,
+      Events    = Matter.Events,
+      Query     = Matter.Query,
+      MouseConstraint = Matter.MouseConstraint,
+      Mouse     = Matter.Mouse;
 
-  $CWSection   = $(".client-work");
-  $CWHeader   = $(".client-work-header");
-  $IHeader    = $(".ideas-header");
+  // create engine
+  var engine    = Engine.create(),
+      world     = engine.world;
 
-  function adjustHeroHeader() {
-    // Main Header
-    if ($body.scrollTop() < 34) {
-      $HeroHeader.height(90 - $body.scrollTop());
-    } else {
-      $HeroHeader.height('56px');
+  // create renderer
+  var render = Render.create({
+    engine: engine,
+    element: document.getElementById("debug"),
+    options: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      background: 'transparent', // transparent to hide
+      wireframeBackground: 'transparent', // transparent to hide
+      hasBounds: false,
+      enabled: true,
+      wireframes: false,
+      showSleeping: true,
+      showDebug: false,
+      showBroadphase: false,
+      showBounds: false,
+      showVelocity: false,
+      showCollisions: false,
+      showAxes: false,
+      showPositions: false,
+      showAngleIndicator: false,
+      showIds: false,
+      showShadows: false
     }
-    if ($body.scrollTop() > 0) {
-      $HeroHeader.addClass('opaque');
-    } else {
-      $HeroHeader.removeClass();
-    }
+  });
+
+  // Disable to hide debug
+  Render.run(render);
+
+  // create runner
+  var runner = Runner.create();
+  Runner.run(runner, engine);
+
+  var ceiling,
+      wallLeft,
+      wallRight,
+      ground;
+
+  // add walls
+  var wallopts = {
+      isStatic:     true,
+      restitution:  0.2,
+      friction:     1
   };
+  var groundopts = {
+      isStatic:     true,
+      restitution:  0,
+      friction:     1
+  };
+  World.add(world, [
+    // ground
+    ground    = Bodies.rectangle(window.innerWidth/2, window.innerHeight+50, window.innerWidth+200, 100, groundopts),
+    // walls
+    ceiling   = Bodies.rectangle(window.innerWidth/2, -50, window.innerWidth+200, 100, wallopts), // top
+    wallRight = Bodies.rectangle(window.innerWidth+50, window.innerHeight/2, 100, window.innerHeight, wallopts), // right
+    wallLeft  = Bodies.rectangle(-50, window.innerHeight/2, 100, window.innerHeight, wallopts) // left
+  ]);
 
-  $('section > header').addClass('locked-bottom');
-
-  function adjustSectionHeaders() {
-    $('section').each(function() {
-      // Section scrolled into viewport
-      if ($(this).offset().top - $body.scrollTop() >= $(window).height() - $(this).find('header').height()) {
-        $(this).find('header').addClass('locked-bottom');
-      } else {
-        $(this).find('header').removeClass('locked-bottom');
+  var bodiesDom = document.querySelectorAll('.strip');
+  var bodies = [];
+  for (var i = 0, l = bodiesDom.length; i < l; i++) {
+    var body = Bodies.rectangle(
+      VIEW.centerX + Math.floor(Math.random() * VIEW.width/2) - VIEW.width/4,
+      0,
+      VIEW.width * bodiesDom[i].offsetWidth / window.innerWidth,
+      VIEW.height * bodiesDom[i].offsetHeight / window.innerHeight, {
+        restitution:      0.2,
+        friction:         1,
+        frictionStatic:   1,
+        density:          1,
+        chamfer:          { radius: 4 },
+        angle:            (Math.random() * 2.000) - 1.000
       }
-      // Section scrolled to top
-      // if ($(this).offset().top - $body.scrollTop() < 0) {
-      //   $(this).find('header').addClass('locked-top');
-      // } else {
-      //   $(this).find('header').removeClass('locked-top');
-      // }
+    );
+    bodiesDom[i].id = body.id;
+    bodies.push(body);
+  }
+  World.add(engine.world, bodies);
+
+  var navsDom = document.querySelectorAll('.page-nav');
+  var navs = [];
+  for (var i = 0, l = navsDom.length; i < l; i++) {
+    var nav = Bodies.circle(
+      VIEW.centerX + Math.floor(Math.random() * VIEW.width/2) - VIEW.width/4,
+      0,
+      24, {
+        restitution:      0.2,
+        friction:         1,
+        frictionStatic:   1,
+        density:          1,
+        angle:            (Math.random() * 2.000) - 1.000
+      }
+    );
+    navsDom[i].id = nav.id;
+    navs.push(nav);
+  }
+  World.add(engine.world, navs);
+
+  // add mouse control
+  var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 1,
+        render: {
+          visible: false
+        }
+      }
     });
+
+  World.add(engine.world, mouseConstraint);
+
+  // keep the mouse in sync with rendering
+  render.mouse = mouse;
+
+  // clicking to post
+
+  var mouseXO,
+      mouseYO,
+      mouseXN,
+      mouseYN;
+
+  function prepareToHandleEvent(e) {
+    mouseXO = e.clientX;
+    mouseYO = e.clientY;
+  }
+
+  function handleEvent(e) {
+    mouseXN = e.clientX;
+    mouseYN = e.clientY;
+    if ((mouseXO == e.clientX) && (mouseYO == e.clientY)) {
+      if (Query.point(bodies, { x: e.clientX, y: e.clientY }).length) {
+        var underMouse = Query.point(bodies, { x: e.clientX, y: e.clientY })[0].id;
+        window.location.href = document.getElementById(underMouse).getAttribute("data-url");
+      }
+      if (Query.point(navs, { x: e.clientX, y: e.clientY }).length) {
+        var underMouse = Query.point(navs, { x: e.clientX, y: e.clientY })[0].id;
+        if (document.getElementById(underMouse).getAttribute("data-url")) {
+          window.location.href = document.getElementById(underMouse).getAttribute("data-url");
+        }
+      }
+    }
+  }
+
+  function prepareToHandleTouchEvent(e) {
+    mouseXO = e.touches[0].clientX;
+    mouseYO = e.touches[0].clientY;
+  }
+
+  function handleTouchEvent(e) {
+    mouseXN = e.changedTouches[0].clientX;
+    mouseYN = e.changedTouches[0].clientY;
+    if ((mouseXO == mouseXN) && (mouseYO == mouseYN)) {
+      if (Query.point(bodies, { x: mouseXN, y: mouseYN }).length) {
+        var underMouse = Query.point(bodies, { x: mouseXN, y: mouseYN })[0].id;
+        window.location.href = document.getElementById(underMouse).getAttribute("data-url");
+      }
+      if (Query.point(navs, { x: mouseXN, y: mouseYN }).length) {
+        var underMouse = Query.point(navs, { x: mouseXN, y: mouseYN })[0].id;
+        if (document.getElementById(underMouse).getAttribute("data-url")) {
+          window.location.href = document.getElementById(underMouse).getAttribute("data-url");
+        }
+      }
+    }
+  }
+
+  document.getElementById("debug").addEventListener('mousedown', function(e) {
+    prepareToHandleEvent(e);
+  });
+  document.getElementById("debug").addEventListener('mouseup', function(e) {
+    handleEvent(e);
+  });
+
+   document.getElementById("debug").addEventListener('touchstart', function(e) {
+    prepareToHandleTouchEvent(e);
+  });
+  document.getElementById("debug").addEventListener('touchend', function(e) {
+    handleTouchEvent(e);
+  });
+
+  window.requestAnimationFrame(update);
+
+  function update() {
+
+    // strips
+    for (var i = 0, l = bodiesDom.length; i < l; i++) {
+      var bodyDom = bodiesDom[i];
+      var body = null;
+      for (var j = 0, k = bodies.length; j < k; j++) {
+        if (bodies[j].id == bodyDom.id) {
+          body = bodies[j];
+          break;
+        }
+      }
+
+      if (body === null) continue;
+
+      bodyDom.style.transform = "translate( " +
+        (body.position.x - bodyDom.offsetWidth / 2) +
+        "px, " +
+        (body.position.y - bodyDom.offsetHeight / 2) +
+        "px )";
+      bodyDom.style.transform += "rotate( " + body.angle + "rad )";
+    }
+
+    // navs
+    for (var i = 0, l = navsDom.length; i < l; i++) {
+      var navDom = navsDom[i];
+      var nav = null;
+      for (var j = 0, k = navs.length; j < k; j++) {
+        if (navs[j].id == navDom.id) {
+          nav = navs[j];
+          break;
+        }
+      }
+
+      if (nav === null) continue;
+
+      navDom.style.transform = "translate( " +
+        (nav.position.x - navDom.offsetWidth / 2) +
+        "px, " +
+        (nav.position.y - navDom.offsetHeight / 2) +
+        "px )";
+      navDom.style.transform += "rotate( " + nav.angle + "rad )";
+    }
+    window.requestAnimationFrame(update);
+  }
+
+}
+
+makeWorld();
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
   };
+}
 
-  $(window).scroll($.throttle(10, adjustHeroHeader));
-  $(window).scroll($.throttle(50, adjustSectionHeaders));
+var refreshWorld = debounce(function() {
+  location.reload();
+}, 500);
 
-  adjustHeroHeader();
-  adjustSectionHeaders();
-
-  // Sticky Goto
-  $(".goto nav").stick_in_parent({
-    offset_top: 167
-  });
-
-
-  //initialize swiper
-  var experienceSwiper = new Swiper ('.experience .swiper-container', {
-    freeMode: true,
-    freeModeSticky: true,
-    freeModeMinimumVelocity: .05,
-    freeModeMomentumRatio: .25,
-    freeModeMomentumVelocityRatio: 1.5,
-    slidesPerView: 'auto',
-    slidesOffsetBefore: 30,
-    slidesOffsetAfter: 30,
-    spaceBetween: 15,
-    grabCursor: true,
-    prevButton: '.swiper-button-prev',
-    nextButton: '.swiper-button-next'
-  });
-  var clientWorkSwiper = new Swiper ('.client-work .swiper-container', {
-    freeMode: true,
-    freeModeSticky: true,
-    freeModeMinimumVelocity: .05,
-    freeModeMomentumRatio: .25,
-    freeModeMomentumVelocityRatio: 1.5,
-    slidesPerView: 'auto',
-    slidesOffsetBefore: 30,
-    slidesOffsetAfter: 30,
-    spaceBetween: 15,
-    pagination: '.swiper-pagination',
-    grabCursor: true,
-    prevButton: '.swiper-button-prev',
-    nextButton: '.swiper-button-next'
-  });
-
-  // Parallax
-  $('.hero').parallaxScroll({
-    friction: 0.5
-  });
-  $(window).trigger('resize');
-
-  // Goto nav builder
-  $('.single article').anchorific({
-    navigation: '.goto nav'
-  });
-
-  // Scroll to
-  $('a[href^="#"]').click(function(e) {
-    e.preventDefault();
-    var duration = Math.abs($(this.hash).offset().top - $body.scrollTop()) * .75;
-    duration = (duration > 1000) ? 1000 : duration;
-    var offset = this.hash == "#client-work" ? $(this).height() * -2 : $(this).height() * -1;
-    $(window).stop(true).scrollTo(this.hash, {
-      duration: duration,
-      offset: offset,
-      interrupt: false
-    });
-  });
-
-});
+window.addEventListener('resize', refreshWorld);
